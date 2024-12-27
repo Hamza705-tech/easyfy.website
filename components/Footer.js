455183152fc7563beaa3839939e58e64c1dc1669
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-27 23:35:50 +0530

    eight

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-t from-teal-700 to-emerald-600 text-white">
       <div className="container px-5 py-3 mx-auto flex justify-center text-center items-center">
         <p className="text-sm">
-          Copyright &copy; easyfy 2024. All rights reserved | Privacy Policy | Terms of Service
+          Copyright &copy; easyfy 2024. All rights reserved | <Link href="/privacypolicy">Privacy Policy</Link>
         </p>
       </div>
     </footer>

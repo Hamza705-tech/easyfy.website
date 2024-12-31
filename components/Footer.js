29ee9bff3abc07ddbb2f7e9d07cb9bd3540af2e3
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-31 20:49:34 +0530

    sixteen

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
     <footer className="bg-gradient-to-t from-teal-700 to-emerald-600 text-white">
       <div className="container px-5 py-3 mx-auto flex justify-center text-center items-center">
         <p className="text-sm">
-          Copyright &copy; easyfy {new Date().getFullYear()}. All rights reserved |&nbsp;</p> <p><Link href="/privacypolicy" className='hover:text-green-300 hover:text-base hover:font-bold transition-all'>Privacy Policy</Link></p>       
+          Copyright &copy; easyfy {new Date().getFullYear()}. All rights reserved |&nbsp;</p>
+           <p><Link href="/privacypolicy" className='hover:text-green-300 hover:text-base hover:font-bold transition-all'>Privacy Policy</Link></p>&nbsp;|&nbsp;
+           <p><Link href="/termsandconditions" className='hover:text-green-300 hover:text-base hover:font-bold transition-all'>Terms & Conditions</Link></p>
       </div>
     </footer>
   )

ef717a3405de850970748f6547eba26a2e5c330c
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-31 21:54:56 +0530

    nineteen

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -31,5 +31,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
+    {
+      url: 'https://www.easyfy.website/termsandconditions',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.easyfy.website/github',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
   ]
 }

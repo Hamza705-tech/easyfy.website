31d4ed963f268fa0eb2e3ba7a8113ed35db1b20c
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-31 19:51:05 +0530

    fifteen

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,6 +1,3 @@
-// https://www.codewithharry.com/blogpost/%60how-to-integrate-mongodb-into-your-nextjs-apps%60/
-// lib/mongodb.js
-
 import { MongoClient } from 'mongodb'
 
 const uri = process.env.MONGODB_URI
@@ -26,36 +23,34 @@ if (process.env.NODE_ENV === 'development') {
   clientPromise = client.connect()
 }
 
-// export default clientPromise
-export default async function handler(req, res) {
+export async function GET() {
   try {
     const client = await clientPromise
     const db = client.db('bitlinks')
 
     const data = await db.collection('url').find({}).toArray()
-    res.status(200).json({ success: true, data })
+    return Response.json({ success: true, data })
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message })
+    return Response.json({ success: false, error: error.message }, { status: 500 })
   }
 }
 
 export async function POST(request) {
+  const body = await request.json() 
+  const client = await clientPromise;
+  const db = client.db("bitlinks")
+  const collection = db.collection("url")
 
-    const body = await request.json() 
-    const client = await clientPromise;
-    const db = client.db("bitlinks")
-    const collection = db.collection("url")
-
-    // Check if the short url exists
-    const doc = await collection.findOne({shorturl: body.shorturl})
-    if(doc){
-        return Response.json({success: false, error: true,  message: 'URL already exists!' })
-    }
-
-    const result = await collection.insertOne({
-        url: body.url,
-        shorturl: body.shorturl
-    })
-
-    return Response.json({success: true, error: false,  message: 'URL Generated Successfully' })
+  // Check if the short url exists
+  const doc = await collection.findOne({shorturl: body.shorturl})
+  if(doc){
+    return Response.json({success: false, error: true,  message: 'URL already exists!' })
   }
+
+  const result = await collection.insertOne({
+    url: body.url,
+    shorturl: body.shorturl
+  })
+
+  return Response.json({success: true, error: false,  message: 'URL Generated Successfully' })
+}

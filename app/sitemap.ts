c4f0d89956aff5173708882b996124093d811584
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-31 22:16:43 +0530

    twenty one

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -43,5 +43,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
+    {
+      url: 'https://www.easyfy.website/LA',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
   ]
 }
